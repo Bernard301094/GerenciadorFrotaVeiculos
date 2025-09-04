4bf38e48812d8add106a18b9ccf9151ab2c72444
Bernard301094 <bernard30101994@gmail.com>
2025-09-04 09:36:55 -0300

    feat(frota): adiciona métodos para gerenciamento de veículos na frota

--- src/br/com/gestaofrota/model/frota/FrotaVeiculos.java
+++ src/br/com/gestaofrota/model/frota/FrotaVeiculos.java
@@ -1,5 +1,7 @@
 package br.com.gestaofrota.model.frota;
 
+import br.com.gestaofrota.enums.StatusVeiculo;
+import br.com.gestaofrota.exception.VeiculoException;
 import br.com.gestaofrota.model.veiculo.Veiculo;
 
 import java.util.List;
@@ -12,5 +14,47 @@ public class FrotaVeiculos {
         this.veiculos = veiculos;
     }
 
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
 
+    public void adicionarVeiculo(Veiculo veiculo) {
+        if (veiculo == null) {
+            throw new VeiculoException("Veículo não pode ser nulo");
+        }
+
+        for(Veiculo v : veiculos) {
+            if (v.getPlaca().equals(veiculo.getPlaca())) {
+                throw new VeiculoException("Placa já cadastrada");
+            }
+        }
+        veiculos.add(veiculo);
+        System.out.println("Veículo adicionado com sucesso");
+    }
+
+    public void removerVeiculo(String placa) {
+
+    }
+
+    public Veiculo buscarVeiculo(String placa) {
+
+        return null;
+    }
+
+    public List<Veiculo> listarPorStatus(StatusVeiculo status) {
+        return null;
+    }
+
+    public double calcularCustoManutencaoTotal() {
+        return 0;
+    }
+
+    public List<Veiculo> obterVeiculosDisponiveis() {
+        return null;
+    }
+
+    public String obterEstatisticas() {
+
+        return "";
+    }
 }
